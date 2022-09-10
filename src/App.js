094906e793c8f0eb6bdfc86cf04f38bdcbaa6629
Kamoliddin Soliev <kamolsf8@gmail.com>
2022-09-10 11:49:54 +0500

    initial state

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "./lib/components/Button";
+import Badge from "./lib/components/Badge";
+function App() {
+  return (
+    <div className="App">
+      <Button label="Enter" kind="primary"></Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+import Badge from "./components/Badge";
+import EIMZO from "./components/e-imzo/Eimzo";
+import Button from "./components/Button";
+export { Badge, Button };
